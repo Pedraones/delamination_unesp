77794134c93d69d3a9200cc3c162498ce66d647f
Pedro Augusto Lopes Gomes <perobr2018@gmail.com>
2025-03-07 21:16:55 -0300

    Transferência do arquivo para a pasta do repositório(como destino final)

---
+++ test.py
@@ -0,0 +1,30 @@
+import cv2
+from cv2 import COLOR_RGB2GRAY, cvtColor
+
+img = cv2.imread('d10.bmp')
+
+img_gray = cvtColor(img, COLOR_RGB2GRAY)
+
+cols = img_gray.shape[1]
+lines = (img_gray.shape[0])
+
+
+#for esta alterando a cor dos pixels dentro do range d ambos os for para preto(0)
+
+for x in range(lines):
+    for y in range(cols):
+        if img_gray[x, y] < 176:
+            img_gray[x, y] = 255
+        
+        else:
+            img_gray[x,y] = 0
+
+#os cv2's abaixo exibem a imagem em uma janela
+
+cv2.namedWindow('kk', cv2.WINDOW_NORMAL)
+cv2.imshow('kk',img_gray)
+
+print('Colunas: ' + str(cols) + ' Linhas: ' + str(lines))
+#cv2.imwrite('saida.png', img) #salva a imagem q esta dentro da variavel IMG
+
+cv2.waitKey()
