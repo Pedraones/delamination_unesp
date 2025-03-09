bbceae5f8d590040251b54f76f30644c1634ac64
Pedro Augusto Lopes Gomes <perobr2018@gmail.com>
2025-03-09 10:35:59 -0300

    Modificação no tamanho da janela de exibição da imagem

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ img_gray = cvtColor(img, COLOR_RGB2GRAY)
 cols = img_gray.shape[1]
 lines = (img_gray.shape[0])
 
+wResi = int(cols / 3)
+hResi = int(lines / 3)
 
 #for esta alterando a cor dos pixels dentro do range d ambos os for para preto(0)
 
@@ -21,10 +23,12 @@ for x in range(lines):
 
 #os cv2's abaixo exibem a imagem em uma janela
 
-cv2.namedWindow('kk', cv2.WINDOW_NORMAL)
-cv2.imshow('kk',img_gray)
+cv2.namedWindow('img_process', cv2.WINDOW_NORMAL)
+cv2.imshow('img_process',img_gray)
+
+cv2.resizeWindow('img_process', wResi,hResi)
 
 print('Colunas: ' + str(cols) + ' Linhas: ' + str(lines))
-#cv2.imwrite('saida.png', img) #salva a imagem q esta dentro da variavel IMG
+#cv2.imwrite('saida.png', img) salva a imagem q esta dentro da variavel IMG
 
 cv2.waitKey()
