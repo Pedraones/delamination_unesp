65969c822c924b27ec053de16e4a199f8ba9b502
Pedro Augusto Lopes Gomes <perobr2018@gmail.com>
2025-03-13 18:45:03 -0300

    feat: Delimitação da borda com sinais de delaminação

--- process_image.py
+++ process_image.py
@@ -3,19 +3,24 @@ from cv2 import COLOR_RGB2GRAY, cvtColor
 
 img = cv2.imread('d10.bmp')
 
-img_resize = img[00:1530, 250:1800]
+img_res = img[00:1530, 250:1800]
 
-cols = img_resize.shape[1]
-lines = img_resize.shape[0]
+cols = img_res.shape[1]
+lines = img_res.shape[0]
   
 center_x = int(cols / 2)
 center_y = int((lines / 2))
-diameter = int((cols / 180)*83)
+
+diam_drill = int((cols / 180)*83.5)
+
+diam_delamina = int(lines/1.97)
 thickness = 2
 
-circle = cv2.circle(img_resize, (center_x, center_y), diameter, (200,200,25), thickness=thickness)
+drilling_circle = cv2.circle(img_res, (center_x, center_y), diam_drill, (200,200,25), thickness=thickness)
 
-img_gray = cvtColor(img_resize, COLOR_RGB2GRAY)
+delamination_circle = cv2.circle(img_res, (center_x, center_y), diam_delamina, (200,200,25), thickness=thickness)
+
+img_gray = cvtColor(img_res, COLOR_RGB2GRAY)
 
 wResi = int(cols / 3)
 hResi = int(lines / 3)
@@ -33,11 +38,11 @@ for x in range(lines):
 #os cv2's abaixo exibem a imagem em uma janela
 
 cv2.namedWindow('img_process', cv2.WINDOW_NORMAL)
-cv2.imshow('img_process', img_resize)
+cv2.imshow('img_process', img_res)
 cv2.resizeWindow('img_process', wResi,hResi)
 
 print('Colunas: ' + str(cols) + ' Linhas: ' + str(lines))
 
-cv2.imwrite('saida.png', img_resize) #salva a imagem q esta dentro da variavel IMG
+#cv2.imwrite('saida.png', img_resize) #salva a imagem q esta dentro da variavel IMG
 
 cv2.waitKey()
