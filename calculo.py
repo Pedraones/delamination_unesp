6d72c6000e94a0c002f4864fcf39d7c09f7730cb
Pedro Augusto Lopes Gomes <perobr2018@gmail.com>
2025-03-14 20:41:30 -0300

    feat: Implementado a lógica de calculo do fator de delaminação convencional

--- calculo.py
+++ calculo.py
@@ -14,9 +14,11 @@ diam_delamina = int(lines/1.97)
 diam_drill_mm = diam_drill / 238.9869
 diam_delamina_mm = diam_delamina / 238.9869
 
-area_less = 2*pi*(diam_delamina_mm/2)
+area_less = 2*pi*(diam_drill_mm/2)
 area_bigger = 2*pi*(diam_delamina_mm/2)
 
-Fd = area_bigger/area_bigger
+Fd = diam_delamina_mm/diam_drill_mm
+Fa = area_bigger/area_less
 
-print("Fator de delaminação comum: " + str(Fd))
+print("Fator de delaminação pelos diâmetros: " + str(Fd))
+print("Fator de delaminação pelas áreas: " + str(Fa))
