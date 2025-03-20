abbd063e4c52535e6440f43e62ea861b812ca9d0
Pedro Augusto Lopes Gomes <perobr2018@gmail.com>
2025-03-20 20:33:52 -0300

    Refactor: Implementado o conceito de orientação a objetos para o arquivo de cálculos

--- calculo.py
+++ calculo.py
@@ -1,24 +1,27 @@
-import cv2
-from cv2 import cvtColor, COLOR_RGB2GRAY
+def measures( cols, lines):
+    cols = cols
+    lines = lines
+    return cols, lines
 
-img = cv2.imread("delaminacao.png")
+def diameters(self):
 
-cols = img.shape[1]
-lines = img.shape[0]
-pi = 3.141592
+    diam_drill = int((self.cols / 180)*83.5)
 
-diam_drill = int((cols / 180)*83.5)
+    diam_delamina = int(self.lines/1.97)
 
-diam_delamina = int(lines/1.97)
+    diam_drill_mm = diam_drill / 238.9869
+    diam_delamina_mm = diam_delamina / 238.9869
 
-diam_drill_mm = diam_drill / 238.9869
-diam_delamina_mm = diam_delamina / 238.9869
+    return diam_delamina_mm, diam_drill_mm
 
-area_less = 2*pi*(diam_drill_mm/2)
-area_bigger = 2*pi*(diam_delamina_mm/2)
+def delamina(self, pi, diam_drill_mm, diam_delamina_mm):
 
-Fd = diam_delamina_mm/diam_drill_mm
-Fa = area_bigger/area_less
+    pi = 3.141592
+    area_less = 2*pi*((diam_drill_mm/2)^2)
+    area_bigger = 2*pi*(diam_delamina_mm/2)
 
-print("Fator de delaminação pelos diâmetros: " + str(Fd))
-print("Fator de delaminação pelas áreas: " + str(Fa))
+    Fd = diam_delamina_mm/diam_drill_mm
+    Fa = area_bigger/area_less
+
+    print("Fator de delaminação pelos diâmetros: " + str(Fd))
+    print("Fator de delaminação pelas áreas: " + str(Fa))
