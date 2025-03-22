cf30c2761b45637039fe6432991391894106fc9f
Pedro Augusto Lopes Gomes <perobr2018@gmail.com>
2025-03-21 21:15:42 -0300

    fix: Chamadas as funções que realizarão a exibição da janela e os cálculos

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
 import image, calculo
+
+diam_drill_delamina = list()
+
+diam_drill_delamina.append(calculo.diameters())
+calculo.delamina(diam_drill_delamina[0][0], diam_drill_delamina[0][1])
+image.window()
