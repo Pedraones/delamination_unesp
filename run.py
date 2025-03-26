d77a228bd23007759c269b7cfd4eb2b8a5821309
Pedro Augusto Lopes Gomes <perobr2018@gmail.com>
2025-03-26 15:55:29 -0300

    feat: Verificação na resposta do usuário se a delaminação está ou não destacada

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
-import image, calculo
-
+import image as img
+import calculo as calc
 
 diam_drill_delamina = list()
 
-image.name
-diam_drill_delamina.append(calculo.diameters())
-calculo.delamina(diam_drill_delamina[0][0], diam_drill_delamina[0][1])
-image.window()
+print('\nA delaminação da imagem está em contraste ou destacada ?')
+respos = input('Digite S se precisar ou N caso contrário \n')
+
+if respos == "S" or respos == "s":
+    name = input('Digite o nome do arquivo: \n')
+
+elif respos == "N" or respos == "n":
+    name = input('Digite o nome do arquivo: \n')
+    img.input_img(name)
+    diam_drill_delamina.append(calc.diameters())
+    calc.delamina(diam_drill_delamina[0][0], diam_drill_delamina[0][1])
+    img.window()
