804ee989541844e4dff79bc3a2477f249821ace0
Pedro Augusto Lopes Gomes <perobr2018@gmail.com>
2025-03-27 19:12:11 -0300

    refactor: Cada delaminação possui uma função para calcular apenas ela (função delamina desmembrada)

--- calculo.py
+++ calculo.py
@@ -1,6 +1,5 @@
 import image as img
 
-
 def diameters():
     cols = img.img_res.shape[1]
     lines = img.img_res.shape[0]
@@ -13,15 +12,19 @@ def diameters():
 
     return diam_drill_mm, diam_delamina_mm
 
-def delamina(diam_drill_mm, diam_delamina_mm):
+def Fd(diam_drill_mm, diam_delamina_mm):
+    Fd = diam_delamina_mm/diam_drill_mm
+
+    return print("\nFator de delaminação pelos diâmetros: " + str(Fd))
+
+def Fa(diam_drill_mm, diam_delamina_mm):
     pi = 3.141592
     ray_drill = diam_drill_mm/2
     ray_delamina = diam_delamina_mm/2
 
     area_less = 2*pi*int(ray_drill**2)
     area_bigger = 2*pi*int(ray_delamina**2)
-
-    Fd = diam_delamina_mm/diam_drill_mm
+    
     Fa = area_bigger/area_less
 
-    return print("\nFator de delaminação pelos diâmetros: " + str(Fd) + "\nFator de delaminação pelas áreas: " + str(Fa))
+    return print("Fator de delaminação pelas áreas: " + str(Fa))
