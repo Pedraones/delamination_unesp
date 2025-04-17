a24a9bc01eea77cc33afcf09b043b949205ca8bc
Pedro Augusto Lopes Gomes <perobr2018@gmail.com>
2025-04-17 19:52:41 -0300

    Remoção da chamada da função measures(), não é realmente necessária

--- image.py
+++ image.py
@@ -9,9 +9,9 @@ def resize():
     img_gray = cvtColor(img, COLOR_RGB2GRAY)
 
     img_res_rgb = img[00:1530, 250:1800]
-    img_res_gray = img[00:1530, 250:1800]
+    img_res_gray = img_gray[00:1530, 250:1800]
 
-    return img_res_rgb, img_res_gray 
+    return img_res_rgb
 
 def input_img(name):
     #armazena a imagem que será utilizada para o "processamento"
@@ -49,7 +49,7 @@ def measures():
 
 def draw_circle():
     #diametro do furo presente
-    diam_drill = int((cols / 180)*83.5)
+    diam_drill = int((lines / 180)*83.5)
 
     #Diametro entre o alcance mais distante da delaminação (manchas mais escuras que estão próximas do furo) 
     diam_delamina = int(lines/1.97)
@@ -58,7 +58,7 @@ def draw_circle():
     thickness = 2
 
     #Circunferência que contorna o "parede interna" do furo, baseada no diametro do furo
-    cv2.circle(img_res_rgb, (center_x, center_y), diam_drill, (200,200,25), thickness=thickness)
+    cv2.circle(img_res_rgb, (center_x, center_y), diam_drill, (0,0,200), thickness=thickness)
 
     #Circunferência que contorna o toda a marca de delaminação em volta do furo, baseada no diâmetro da delaminação (diam_delamina)
     cv2.circle(img_res_rgb, (center_x, center_y), diam_delamina, (200,200,25), thickness=thickness)
@@ -67,15 +67,9 @@ def draw_circle():
 
 #Exibição da imagem em uma janela
 def window():
-    measures()
     draw_circle()
     cv2.namedWindow('img_process', cv2.WINDOW_NORMAL)
     cv2.imshow('img_process', img_res_rgb)
     cv2.resizeWindow('img_process', wResi,hResi)
 
-    print('\nColunas: ' + str(cols) + ' Linhas: ' + str(lines))
-
-    #cv2.imwrite('saida.png', img_resize) #salva a imagem q esta dentro da variavel IMG
-
-    #Fecha a janela ao clicar em qualquer tecla
     cv2.waitKey()
