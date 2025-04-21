97f15a9bb7da712e6a0148cf7bef34790d435a26
Pedro Augusto Lopes Gomes <perobr2018@gmail.com>
2025-04-21 18:56:29 -0300

    feat: Função que padroniza os pixels da imagens, em apenas pretos e brancos

--- image.py
+++ image.py
@@ -9,9 +9,9 @@ def resize():
     img_gray = cvtColor(img, COLOR_RGB2GRAY)
 
     img_res_rgb = img[00:1530, 250:1800]
-    img_res_gray = img[00:1530, 250:1800]
+    img_res_gray = img_gray[00:1530, 250:1800]
 
-    return img_res_rgb, img_res_gray 
+    return img_res_rgb
 
 def input_img(name):
     #armazena a imagem que será utilizada para o "processamento"
@@ -47,9 +47,23 @@ def measures():
 
     return cols, lines
 
+def analyzes_px():
+    measures()        
+    global img_res_gray
+
+    for x in range(lines):
+        for y in range(cols):
+            if img_res_gray[x, y] < 190:
+                img_res_gray[x, y] = 0
+
+            if img_res_gray[x, y] > 210:
+                img_res_gray[x, y] = 255
+                
+    return img_res_gray
+
 def draw_circle():
     #diametro do furo presente
-    diam_drill = int((cols / 180)*83.5)
+    diam_drill = int((lines / 180)*83.5)
 
     #Diametro entre o alcance mais distante da delaminação (manchas mais escuras que estão próximas do furo) 
     diam_delamina = int(lines/1.97)
@@ -58,7 +72,7 @@ def draw_circle():
     thickness = 2
 
     #Circunferência que contorna o "parede interna" do furo, baseada no diametro do furo
-    cv2.circle(img_res_rgb, (center_x, center_y), diam_drill, (200,200,25), thickness=thickness)
+    cv2.circle(img_res_rgb, (center_x, center_y), diam_drill, (0,0,200), thickness=thickness)
 
     #Circunferência que contorna o toda a marca de delaminação em volta do furo, baseada no diâmetro da delaminação (diam_delamina)
     cv2.circle(img_res_rgb, (center_x, center_y), diam_delamina, (200,200,25), thickness=thickness)
@@ -73,9 +87,4 @@ def window():
     cv2.imshow('img_process', img_res_rgb)
     cv2.resizeWindow('img_process', wResi,hResi)
 
-    print('\nColunas: ' + str(cols) + ' Linhas: ' + str(lines))
-
-    #cv2.imwrite('saida.png', img_resize) #salva a imagem q esta dentro da variavel IMG
-
-    #Fecha a janela ao clicar em qualquer tecla
     cv2.waitKey()
