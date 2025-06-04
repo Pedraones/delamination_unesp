482497c02e05939c5c76b62772646bf6f56d2a2b
Pedro Augusto Lopes Gomes <perobr2018@gmail.com>
2025-06-04 04:52:20 -0300

    Remove: Simplificação na passagem da imagem, pois todos os dados sobre a delaminação dela já serão retornados

--- run.py
+++ run.py
@@ -3,31 +3,12 @@ import calculo as calc
 
 diam_drill_delamina = list()
 
-print('\nA delaminação da imagem está em contraste ou destacada ?')
-respos = str.lower(input('Digite S se estiver ou N caso contrário \n'))
-
-if respos == "s":
-    #name = input('Digite o nome do arquivo: \n')
-    name = "fEntrada/d1.bmp"
-    img.input_img(name)
-    global color
-    color = str.lower(input('\nOs sinais de delaminação está com a cor preta ou branca ? \n'))
-
-    if color == "preta" or color == "preto":
-        calc.count_preto()
-        calc.delamina_for_px(color)
-
-    elif color == "branca" or color == "branco":
-        calc.count_branco()
-        calc.delamina_for_px(color)
-
-elif respos == "n":
-    #name = input('Digite o nome do arquivo: \n')
-    name = "fEntrada/d1.bmp"
-    img.input_img(name)
-    diametros = calc.diameters()
-    diam_drill_delamina.append(diametros[0])
-    diam_drill_delamina.append(diametros[1])
-    calc.Fd(diam_drill_delamina[0], diam_drill_delamina[1])
-    calc.Fa(diam_drill_delamina[0], diam_drill_delamina[1])
-    img.window()
+#name = input('Digite o nome do arquivo: \n')
+name = "fEntrada/d1.bmp"
+img.input_img(name)
+diametros = calc.diameters()
+diam_drill_delamina.append(diametros[0])
+diam_drill_delamina.append(diametros[1])
+calc.Fd(diam_drill_delamina[0], diam_drill_delamina[1])
+calc.Fa(diam_drill_delamina[0], diam_drill_delamina[1])
+img.window()
